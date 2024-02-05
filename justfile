9a6dd79c04c591c1d0ff755021918c84a2129ce8
Silen Locatelli <88315530+SilenLoc@users.noreply.github.com>
2024-02-05 12:55:14 +0100

    intial

---
+++ justfile
@@ -0,0 +1,64 @@
+
+set dotenv-load
+
+alias r := run
+
+bt := '0'
+
+log := "warn"
+
+export JUST_LOG := log
+export ROCKET_PORT := "3721"
+
+frontend *args:
+    cd frontend/tbtt && just {{args}}
+
+hurl_opts := "--variables-file hurl.env.test --test"
+
+@_list:
+	just --list --unsorted
+
+# Perform all verifications (compile, test, lint, etc.)
+verify: test shutdown run-release api-test lint
+
+# Run the service locally (from sources)
+run:
+	cargo-shuttle run --port 3721
+
+
+# Run the tests
+test:
+	cargo hack test --feature-powerset --locked
+	cargo deny check licenses
+
+# Run the static code analysis
+lint:
+	cargo fmt --all -- --check
+	cargo hack clippy --feature-powerset --all-targets --workspace --locked
+	cargo deny check
+
+wait-for-api:
+	hurl api/health.hurl --retry 60 {{hurl_opts}}
+
+
+# run acceptance tests against the running test stack
+api-test *args: wait-for-api
+    hurl api/*.hurl {{hurl_opts}} {{args}}
+
+# Install cargo dev-tools used by the `verify` recipe (requires rustup to be already installed)
+install-dev-tools:
+	rustup install stable
+	rustup override set stable
+	cargo install cargo-hack cargo-watch cargo-deny hurl
+
+
+fmt:
+  cargo fmt
+
+
+shutdown:
+    lsof -t -i:{{ROCKET_PORT}} | xargs -r kill
+
+run-release: shutdown
+	just frontend build
+    just run &
---
+++ src/main.rs
@@ -0,0 +1,89 @@
+#![deny(future_incompatible, nonstandard_style, unsafe_code, rust_2018_idioms)]
+#![warn(clippy::pedantic)]
+#![allow(
+    clippy::single_match_else,
+    clippy::let_unit_value,
+    clippy::no_effect_underscore_binding,
+    clippy::needless_pass_by_value,
+    clippy::wildcard_imports,
+    clippy::module_name_repetitions,
+    clippy::unused_async,
+    clippy::items_after_test_module
+)]
+
+#[macro_use]
+extern crate rocket;
+#[cfg(test)]
+#[macro_use]
+extern crate rstest;
+
+use std::path::Path;
+
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::fs::NamedFile;
+use rocket::http::Header;
+use rocket::{routes, Request, Response};
+
+#[get("/healthz")]
+fn healthz() {}
+
+#[options("/<_..>")]
+fn all_options() {
+    /* Intentionally left empty */
+}
+
+#[get("/")]
+async fn index() -> NamedFile {
+  let page_directory_path = 
+  format!("{}/../frontend/tbtt/dist", env!("CARGO_MANIFEST_DIR"));
+  NamedFile::open(Path::new(&page_directory_path).join("index.html")).await.unwrap()
+}
+
+#[shuttle_runtime::main]
+async fn rocket() -> shuttle_rocket::ShuttleRocket {
+    let rocket = rocket::build()
+        .mount("/api", routes![healthz])
+        .mount("/", routes![index])
+        .attach(CORS);
+
+    Ok(rocket.into())
+}
+
+#[cfg(test)]
+mod tests {
+    use rocket::{http::Status, local::blocking::Client};
+
+    use super::*;
+
+    #[fixture]
+    fn client() -> Client {
+        Client::tracked(rocket::build().mount("/", routes![healthz])).unwrap()
+    }
+
+    #[rstest]
+    fn healthz_returns_ok(client: Client) {
+        assert_eq!(Status::Ok, client.get("/healthz").dispatch().status());
+    }
+}
+
+pub struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response,
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Methods",
+            "POST, GET, PATCH, OPTIONS",
+        ));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
